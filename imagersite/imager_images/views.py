630b1f1ede412157b3afc86d357b9a89abb2f0bc
Loaye <jr.carstens00@gmail.com>
2017-12-08 14:25:24 -0800

    adds edit views to photos and albums

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -34,7 +34,41 @@ class AlbumView(ListView):
 
     model = Album
     template_name = 'imager_images/album.html'
-    # def get_context_data(self, pk=None):
-    #     """Get context data for view."""
-    #     album = Album.objects.get(id=pk)
-    #     return {'album': album}
+  
+class EditAlbum(CreateView):
+    """View for adding a album."""
+
+    model = Album
+    form_class = AlbumForm
+
+    success_url = reverse_lazy('library')
+
+    def form_valid(self, form):
+        """."""
+        if self.request.user.is_authenticated:
+            form.instance.user = self.request.user
+            self.object = form.save()
+            return super(AddAlbum, self).form_valid(form)
+        else:
+            raise Http404()
+
+class EditImage(CreateView):
+    """View for adding a image."""
+
+    model = Photo
+    fields = [
+        'image',
+        'title',
+        'description',
+        'published'
+    ]
+    success_url = reverse_lazy('library')
+
+    def form_valid(self, form):
+        """."""
+        if self.request.user.is_authenticated:
+            form.instance.user = self.request.user
+            self.object = form.save()
+            return super(AddImage, self).form_valid(form)
+        else:
+            raise Http404()
